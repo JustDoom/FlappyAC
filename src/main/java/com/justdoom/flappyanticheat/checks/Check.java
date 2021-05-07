4adeaba057279038c1c621f5b207f5d286ce3828
BigFatAlt <64555053+BigFatAlt@users.noreply.github.com>
2021-05-08 00:27:16 +0100

    fixed issues
    went back to original checkmanager as that was the main issue

--- src/main/java/com/justdoom/flappyanticheat/checks/Check.java
+++ src/main/java/com/justdoom/flappyanticheat/checks/Check.java
@@ -2,32 +2,34 @@ package com.justdoom.flappyanticheat.checks;
 
 import com.justdoom.flappyanticheat.FlappyAnticheat;
 import com.justdoom.flappyanticheat.data.PlayerData;
+import com.justdoom.flappyanticheat.utils.Color;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Check <E extends Event>{
+public class Check {
 
     private String check, checkType;
     private boolean experimental;
 
-    private CheckData checkData;
+    public CheckData checkData;
 
     public final PlayerData data;
 
     public Check(PlayerData data) {
         this.data = data;
 
+        this.checkData = getClass().getAnnotation(CheckData.class);
+
         if (!getClass().isAnnotationPresent(CheckData.class)) {
-            Bukkit.getServer().getConsoleSender().sendMessage(Color.RED + "CheckData missing in " + getClass().getSimpleName());
+            Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.RED + "CheckData missing in " + getClass().getSimpleName());
         }
 
         this.check = checkData.name();
@@ -41,13 +43,12 @@ public class Check <E extends Event>{
         String flagmsg = FlappyAnticheat.getInstance().getConfig().getString("prefix") + " " + FlappyAnticheat.getInstance().getConfig().getString("alerts.failed-check");
         flagmsg = flagmsg.replace("%player%", player.getName()).replace("%check%", this.check + " " + checkType);
 
-        TextComponent component = new TextComponent(flagmsg);
+        TextComponent component = new TextComponent(Color.translate(flagmsg));
         component.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(debug).create()));
 
-        //I did not test this so test it to find out if it works if it does not contact me
         FlappyAnticheat.getInstance().dataManager.dataMap.values()
                 .stream().filter(playerData -> data.player.hasPermission("flappyanticheat.alerts"))
-                .forEach(playerData -> playerData.player.spigot().sendMessage(ChatMessageType.valueOf(ChatColor.translateAlternateColorCodes('&', String.valueOf(component)))));
+                .forEach(playerData -> playerData.player.spigot().sendMessage(component));
 
     }
 
