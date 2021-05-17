5330a035d7f781166081e6377cb05f9885d7db55
PixchureLive <74546734+Pixchure@users.noreply.github.com>
2021-05-17 08:50:37 -0400

    Update CheckManager.java

--- src/main/java/com/justdoom/flappyanticheat/checks/CheckManager.java
+++ src/main/java/com/justdoom/flappyanticheat/checks/CheckManager.java
@@ -6,6 +6,7 @@ import com.justdoom.flappyanticheat.checks.movement.groundspoof.GroundSpoofA;
 import com.justdoom.flappyanticheat.checks.movement.noslow.NoSlowA;
 import com.justdoom.flappyanticheat.checks.player.badpackets.BadPacketsA;
 import com.justdoom.flappyanticheat.checks.player.badpackets.BadPacketsB;
+import com.justdoom.flappyanticheat.checks.combat.KillAuraA;
 import com.justdoom.flappyanticheat.data.PlayerData;
 import io.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.event.PacketListener;
