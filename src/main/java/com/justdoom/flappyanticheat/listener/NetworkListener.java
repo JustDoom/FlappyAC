f03adb10ecab3616e028f1e51a3aae0cf70aedc4
BuildTools <unconfigured@null.spigotmc.org>
2021-08-27 20:11:00 +1000

    Stop packets being too big

--- src/main/java/com/justdoom/flappyanticheat/listener/NetworkListener.java
+++ src/main/java/com/justdoom/flappyanticheat/listener/NetworkListener.java
@@ -5,6 +5,9 @@ import com.justdoom.flappyanticheat.data.FlappyPlayer;
 import com.justdoom.flappyanticheat.packet.Packet;
 import io.github.retrooper.packetevents.event.PacketListenerAbstract;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.packettype.PacketType;
+import io.github.retrooper.packetevents.packetwrappers.play.in.flying.WrappedPacketInFlying;
+import org.bukkit.Bukkit;
 
 public class NetworkListener extends PacketListenerAbstract {
 
@@ -13,6 +16,19 @@ public class NetworkListener extends PacketListenerAbstract {
         final FlappyPlayer player = FlappyAnticheat.INSTANCE.getDataManager().getData(event.getPlayer());
 
         if (player == null) return;
+
+        if (PacketType.Play.Client.Util.isInstanceOfFlying(event.getPacketId())) {
+            final WrappedPacketInFlying wrapper = new WrappedPacketInFlying(event.getNMSPacket());
+
+            if (Math.abs(wrapper.getX()) > 1.0E+7
+                    || Math.abs(wrapper.getY()) > 1.0E+7
+                    || Math.abs(wrapper.getZ()) > 1.0E+7
+                    || Math.abs(wrapper.getPitch()) > 1.0E+7
+                    || Math.abs(wrapper.getYaw()) > 1.0E+7) {
+                Bukkit.getScheduler().runTask(FlappyAnticheat.INSTANCE.getPlugin(), () -> event.getPlayer().kickPlayer("No"));
+                return;
+            }
+        }
 
         FlappyAnticheat.INSTANCE.getPacketExecutor().execute(() -> FlappyAnticheat.INSTANCE.getReceivingPacketProcessor()
                 .handle(player, new Packet(event.getNMSPacket(), event.getPacketId())));
