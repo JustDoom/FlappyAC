a757b40fcf7b8b153c16b2566695943c30685fa9
BuildTools <unconfigured@null.spigotmc.org>
2021-08-27 18:34:33 +1000

    make nofall a and criticals a much more efficient

--- src/main/java/com/justdoom/flappyanticheat/checks/movement/nofall/NoFallA.java
+++ src/main/java/com/justdoom/flappyanticheat/checks/movement/nofall/NoFallA.java
@@ -30,48 +30,15 @@ public class NoFallA extends Check {
         if(!packet.isPosition()
                 && !packet.isLook()
                 && !packet.isPositionLook()
-                || isExempt(ExemptType.GAMEMODE, ExemptType.TPS, ExemptType.JOINED)) return;
+                || isExempt(ExemptType.GAMEMODE, ExemptType.TPS, ExemptType.JOINED,
+                ExemptType.PISTON, ExemptType.SHULKER, ExemptType.VEHICLE)) return;
 
         double groundY = 0.015625;
-        boolean client = player.getPositionProcessor().isOnGround(), server = player.getPositionProcessor().getY() % groundY < 0.0001;;
+        boolean client = data.getPositionProcessor().isOnGround(), server = data.getPositionProcessor().getY() % groundY < 0.0001;;
 
-        if (client && !server && !PlayerUtil.isOnClimbable(player.getPlayer())) {
+        if (client && !server && !PlayerUtil.isOnClimbable(data.getPlayer())) {
             if (++buffer > 1) {
-                boolean boat = false, shulker = false, pistonHead = false;
-
-                AtomicReference<List<Entity>> nearby = new AtomicReference<>();
-                sync(() -> nearby.set(player.getPlayer().getNearbyEntities(1.5, 10, 1.5)));
-
-                for (Entity entity : nearby.get()) {
-                    if (entity.getType() == EntityType.BOAT && player.getPositionProcessor().getY() > entity.getLocation().getY()) {
-                        boat = true;
-                        break;
-                    }
-
-                    if (entity.getType() == EntityType.SHULKER && player.getPositionProcessor().getY() > entity.getBoundingBox().getMinY()) {
-                        shulker = true;
-                        break;
-                    }
-                }
-
-                for (Block block : PlayerUtil.getNearbyBlocks(new Location(player.getPlayer().getWorld(), player.getPositionProcessor().getX(), player.getPositionProcessor().getY(), player.getPositionProcessor().getZ()), 2)) {
-
-                    //TODO: exempt all types of shulker boxes
-                    if(block.getType() == XMaterial.SHULKER_BOX.parseMaterial()){
-                        shulker = true;
-                        break;
-                    }
-
-                    if (block.getType() == XMaterial.PISTON_HEAD.parseMaterial()) {
-                        pistonHead = true;
-                        break;
-                    }
-                }
-
-                if (!boat && !shulker && !pistonHead) {
-
-                    fail("No Info");
-                }
+                fail("No Info");
             }
         } else if (buffer > 0) buffer-=0.5;
     }
