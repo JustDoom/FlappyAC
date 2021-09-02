b252a02ec5ae071a11deb390b37625a3d6b66178
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-09-02 19:32:20 +1000

    Update PlayerUtil.java

--- src/main/java/com/justdoom/flappyanticheat/util/PlayerUtil.java
+++ src/main/java/com/justdoom/flappyanticheat/util/PlayerUtil.java
@@ -41,50 +41,6 @@ public final class PlayerUtil {
         return player.getLocation().getBlock().getType() == Material.LADDER || player.getLocation().getBlock().getType() == Material.VINE;
     }
 
-    public static Set<Block> getNearbyBlocksHorizontally(final Location location, final int radius) {
-        final Set<Block> blocks = new HashSet<>();
-
-        for (int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
-            for (int y = location.getBlockY(); y <= location.getBlockY(); y++) {
-                for (int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
-                    blocks.add(location.getWorld().getBlockAt(x, y, z));
-                }
-            }
-        }
-
-        return blocks;
-    }
-
-    //im sorry for this, but this is the best botch i have LMFAO
-    //since i cant use the regular method and not expand vertical radius, just remove the radius in a new util ;)
-    // - Spiriten
-
-    public static Set<Block> getNearbyBlocks(Location location, final int radius) {
-        final Set<Block> blocks = new HashSet<>();
-
-        location = location.clone();
-
-        for (int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
-            for (int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
-                for (int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
-                    blocks.add(location.getWorld().getBlockAt(x, y, z));
-                }
-            }
-        }
-
-        return blocks;
-    }
-
-    public static boolean isFlying(final Player player) {
-        return player.isFlying();
-    }
-
-    public static double getDistanceBB(final BoundingBox origin, final Vector attacker) {
-        final double x = Math.min(Math.pow(attacker.getX() - origin.getMinX(), 2.0), Math.pow(attacker.getX() - origin.getMaxX(), 2.0));
-        final double z = Math.min(Math.pow(attacker.getZ() - origin.getMinZ(), 2.0), Math.pow(attacker.getZ() - origin.getMaxZ(), 2.0));
-        return Math.sqrt(x + z);
-    }
-
     //From AntiHaxerMan
     public static List<Entity> getEntitiesWithinRadius(final Location location, final double radius) {
         try {
@@ -127,19 +83,6 @@ public final class PlayerUtil {
         }
 
         return null;
-    }
-
-    public boolean isInLiquid(final Player player) {
-        final double expand = 0.31;
-        final Location location = player.getLocation();
-        for (double x = -expand; x <= expand; x += expand) {
-            for (double z = -expand; z <= expand; z += expand) {
-                if (player.getWorld().getBlockAt(location.clone().add(x, -0.5001, z)).isLiquid()) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     public int getPotionLevel(final Player player, final PotionEffectType effect) {
