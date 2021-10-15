64d00f360ecd1998553e4b16cd8c059a96cf0a5b
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-10-15 16:43:22 +1000

    clean up code a little

--- plugin/src/main/java/com/imjustdoom/flappyanticheat/checks/movement/speed/SpeedA.java
+++ plugin/src/main/java/com/imjustdoom/flappyanticheat/checks/movement/speed/SpeedA.java
@@ -18,9 +18,8 @@ public class SpeedA extends Check {
     public void handle(final Packet packet) {
 
         if (!packet.isPosition() && !packet.isPositionLook()
-                || data.getPlayer().isFlying()
                 || data.getPositionProcessor().getY() == data.getPositionProcessor().getLastY()
-                || isExempt(ExemptType.JOINED, ExemptType.ENTITIES/**ExemptType.WEB, ExemptType.TELEPORT**/)) return;
+                || isExempt(ExemptType.JOINED, ExemptType.ENTITIES, ExemptType.FLYING/**ExemptType.WEB, ExemptType.TELEPORT**/)) return;
 
 
         if (!data.getPositionProcessor().isOnGround() && !data.getPositionProcessor().isLastOnGround()) {
