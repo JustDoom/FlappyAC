8ec7f7f6eac9e2b4b33ac2d8e381fbf6d2dca75d
BuildTools <unconfigured@null.spigotmc.org>
2021-10-21 17:51:18 +1000

    Fix SpeedA false when joining and not moving

--- plugin/src/main/java/com/imjustdoom/flappyanticheat/packet/processor/ReceivingPacketProcessor.java
+++ plugin/src/main/java/com/imjustdoom/flappyanticheat/packet/processor/ReceivingPacketProcessor.java
@@ -31,7 +31,7 @@ public class ReceivingPacketProcessor {
             }
 
             if(packet.isPosition() || packet.isPositionLook()) {
-                if (wrapper.getX() != pos.getX() || wrapper.getY() != pos.getY() || wrapper.getZ() != pos.getZ())
+                if (wrapper.getX() != pos.getX() || wrapper.getY() != pos.getY() || wrapper.getZ() != pos.getZ() || wrapper.isOnGround() != pos.isOnGround())
                     data.getPositionProcessor().handle(wrapper.getX(), wrapper.getY(), wrapper.getZ(), wrapper.isOnGround());
             }
         }
