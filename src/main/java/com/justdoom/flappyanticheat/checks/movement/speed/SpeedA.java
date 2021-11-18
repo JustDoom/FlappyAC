3ad6039a00fa0677771ca3b0b4f3375a677b4795
BuildTools <unconfigured@null.spigotmc.org>
2021-11-18 15:44:43 +1000

    Update SpeedA.java

--- src/main/java/com/justdoom/flappyanticheat/checks/movement/speed/SpeedA.java
+++ src/main/java/com/justdoom/flappyanticheat/checks/movement/speed/SpeedA.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,7 +32,7 @@ public class SpeedA extends Check implements Listener {
         PlayerData data = FlappyAnticheat.getInstance().dataManager.getData(event.getPlayer().getUniqueId());
 
         //dont run the check if they have /fly on or are creative flying
-        if (player.isFlying() || player.isGliding()) return;
+        if (player.isFlying() || player.isGliding() || player.hasPotionEffect(PotionEffectType.DOLPHINS_GRACE)) return;
         if(PacketEvents.get().getPlayerUtils().isGeyserPlayer(event.getPlayer().getPlayer())) return;
 
         //pos stuff
